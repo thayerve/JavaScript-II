ac5743cde52232cd937c3580b64f941fb25d37ad
Virginia Thayer <virginia@stripe.com>
2019-06-11 14:26:47 -0700

    completed Array Methods and Callbacks challenges

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,5 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
+
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
@@ -27,24 +28,55 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length){
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(last){
+  console.log(last);
+})
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(10, 3, function(sum){
+  console.log(sum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(6, 4, function(product){
+  console.log(product);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)){
+    return cb(true);
+  } else { 
+    return cb(false);
+  };
 }
+
+contains ('Gum', items, function(doesContain){
+  console.log(doesContain);
+})
+
 
 /* STRETCH PROBLEM */
 
